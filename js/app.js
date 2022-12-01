399e45707d73a7c1e301bf75d1f8fe5b6a838c91
aditi2306 <35637439+aditi2306@users.noreply.github.com>
2022-12-01 20:48:54 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -30,3 +30,26 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  addBtn.style.display = "block";
+
+  addBtn.addEventListener("click", (e) => {
+    addBtn.style.display = "none";
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
--- serviceWorker.js
+++ serviceWorker.js
@@ -7,9 +7,7 @@ const assets = [
   "/images/logo.jpg"
 ];
 
-let deferredPrompt;
-const addBtn = document.querySelector(".add-button");
-addBtn.style.display = "none";
+
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
@@ -27,21 +25,3 @@ self.addEventListener("fetch", fetchEvent => {
   );
 });
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  addBtn.style.display = "block";
-
-  addBtn.addEventListener("click", (e) => {
-    addBtn.style.display = "none";
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
-      deferredPrompt = null;
-    });
-  });
-});
