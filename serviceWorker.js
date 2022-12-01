05d3f21ef59498a2bf1193322ff0db3c1d288bd1
aditi2306 <35637439+aditi2306@users.noreply.github.com>
2022-12-01 19:48:52 +0530

    root folder files

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDev = "dev-employee-pwc-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/logo.jpg"
+];
+const test =document.getElementById("install-id");
+
+test.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
