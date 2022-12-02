de8001fd8ca692096e34267b5a4b49f768a8b536
aditi2306 <35637439+aditi2306@users.noreply.github.com>
2022-12-02 11:34:18 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,26 +1,3 @@
-const container = document.querySelector(".container");
-const coffees = [
-  {
-    name: "",
-    image: "images/logo.jpg"
-  }
-];
-const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
-};
-
-document.addEventListener("DOMContentLoaded", showCoffees);
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
